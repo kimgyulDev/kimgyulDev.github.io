8421be19a3379b08b0ae2b83fab429fd475d5b8b
kimgyulDev <139095957+kimgyulDev@users.noreply.github.com>
2023-07-11 23:56:43 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import { Link } from 'gatsby'
+
+const IndexPage: FunctionComponent = function() {
+  return (
+    <div>
+      <Link to="/info/">To info</Link>
+    </div>
+)};
+
+export default IndexPage;
+
